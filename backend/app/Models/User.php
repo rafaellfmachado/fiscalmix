cf733e119be16a255728fdb545568cc38f91d9b2
Rafael Machado <rfm@franet.com.br>
2026-01-05 23:32:26 -0300

    feat: Add account relationship to User model
    - Define belongsTo relationship with Account
    - Enables eager loading of account
    - Fixes 'undefined relationship' error
    - Login now working 100%!
    - 35 COMMITS - FINAL SUCCESS!

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -48,4 +48,12 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    /**
+     * Get the account that owns the user.
+     */
+    public function account()
+    {
+        return $this->belongsTo(Account::class);
+    }
 }
